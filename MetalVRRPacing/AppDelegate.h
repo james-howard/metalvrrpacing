978de804fe87ed1a8b989bdfae816df53253dada
James Howard <jameshoward@mac.com>
2023-07-19 17:27:07 -0700

    Initial Commit

---
+++ MetalVRRPacing/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MetalVRRPacing
+//
+//  Created by James Howard on 7/19/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MetalVRRPacing/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  MetalVRRPacing
+//
+//  Created by James Howard on 7/19/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
